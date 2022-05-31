dcfe9a8572b3673ada90cd14977375bb1f19b0f8
Heloísa Tambara <81993336+heloisatambara@users.noreply.github.com>
2022-05-31 15:45:32 -0300

    Update clusters.R

--- clusters.R
+++ clusters.R
@@ -4,12 +4,13 @@
 #### exercicio a) ####
 # get the data
 df = data.frame(read.csv('E:/usp/estudos/estat/mae399/201806-citibike-tripdata.csv'))
+df = df[, c('start.station.id', 'end.station.id', 'start.station.latitude', 'start.station.longitude', 'end.station.latitude','end.station.longitude' )]
 
 # get the msot frequent start station
 mostFreqStart = names(sort(table(df$start.station.id), decreasing=T)[1])
 
 # data frame with only the most frequent station
-df2 = df[df$start.station.id == mostFreqStart, c('start.station.id', 'end.station.id')]
+df2 = df[df$start.station.id == mostFreqStart,]
 
 # get the 9 most frequent end stations
 mostFreqEnd = names(sort(table(df2$end.station.id), decreasing=T)[1:9])
@@ -18,8 +19,10 @@ mostFreqEnd = names(sort(table(df2$end.station.id), decreasing=T)[1:9])
 stations = sort(c(mostFreqEnd, mostFreqStart))
 
 # data frame with only the 10 selected stations
-df2 = df[df$start.station.id %in% stations, c('start.station.id', 'end.station.id')]
+df2 = df[df$start.station.id %in% stations,]
 df2 = df2[df2$end.station.id %in% stations,]
+long1 = unique(df2$start.station.longitude)
+lat1 = unique(df2$start.station.latitude) 
 
 #### exercicio b) ####
 
@@ -36,24 +39,27 @@ length(unique(df$start.station.id))
 
 # retirar ocorrencias diferentes das stations
 `%!in%` <- Negate(`%in%`)
-df3 = df[df$start.station.id %!in% stations & df$end.station.id %!in% stations , c('start.station.id', 'end.station.id')]
+df3 = df[df$start.station.id %!in% stations & df$end.station.id %!in% stations,]
 
 # refazer tudo para novo cluster
 # c.a
 mostFreqStart = names(sort(table(df3$start.station.id), decreasing=T)[1])
-df2 = df3[df3$start.station.id == mostFreqStart, c('start.station.id', 'end.station.id')]
+df2 = df3[df3$start.station.id == mostFreqStart,]
 mostFreqEnd = names(sort(table(df2$end.station.id), decreasing=T)[1:10]) # aqui peguei 10 pois a mostFreqStart está em mostFreqEnd
 stations = sort(c(mostFreqEnd)) # somente o mostFreqEnd pois contem mosFreqStart
-df2 = df3[df3$start.station.id %in% stations, c('start.station.id', 'end.station.id')]
+df2 = df3[df3$start.station.id %in% stations,]
 df2 = df2[df2$end.station.id %in% stations,]
+long2 = unique(df2$start.station.longitude)
+lat2 = unique(df2$start.station.latitude) 
 # c.b
 cluster2 = matrix(prop.table(table(df2$start.station.id, df2$end.station.id)), nrow = 10)
 colnames(cluster2) = stations
 rownames(cluster2) = stations
 cluster2
-# c.c
-df3 = df3[df3$start.station.id %!in% stations & df3$end.station.id %!in% stations , c('start.station.id', 'end.station.id')]
-# rodar c.a de novo
+
+#### c.c mais clusters ####
+df3 = df3[df3$start.station.id %!in% stations & df3$end.station.id %!in% stations,]
+# rodar linha 57 e c.a a cada novo cluster
 cluster3 = matrix(prop.table(table(df2$start.station.id, df2$end.station.id)), nrow = 10)
 colnames(cluster3) = stations
 rownames(cluster3) = stations
@@ -71,4 +77,14 @@ cluster5
 
 # quantos clusters tem que fazer?
 
-cbind()
+
+#### exercicio d) ####
+library(mapview)
+
+# maybe get unique long and lat
+
+FirstCluster = data.frame(longitude =long1, latitude = lat1, cluster = 1)
+SecondCluster = data.frame(longitude = long2, latitude = lat2, cluster = 2)
+clusters = rbind(FirstCluster, SecondCluster)
+mapview(clusters, xcol = "longitude", ycol = "latitude", grid = FALSE, crs = 4269, zcol = 'cluster')
+  
