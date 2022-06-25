24b4f64b6a06eb0d0e299f877dbab689deabfb69
Heloísa Tambara <81993336+heloisatambara@users.noreply.github.com>
2022-06-25 07:55:22 -0300

    Update lista7.R

--- lista7.R
+++ lista7.R
@@ -18,13 +18,28 @@ q = 1-p
 
 ## Random walk
 x = X[1]
-soma = 0
-for (i in 1000) {
-  while (0 > x & x > 50) {
-    result = sample(c(-1,1), 1, replace = TRUE, prob = c(20/38, 18/38))
-    x = x + result
-  }
-  if (x == 50) {
-    soma = soma + 1
+soma = c(0,0,0,0)
+soma2 = 0
+bool = 1
+for (i in 1:4){
+  for (j in 1:1000) {
+    x = X[i]
+    bool = 1
+    while (bool) {
+      result = sample(c(-1,1), 1, replace = TRUE, prob = c(20/38, 18/38))
+      x = x + result
+      if (x==0) {
+        soma2 = soma2 + 1 # para conferir
+        bool = 0
+      }
+      if (x==50) {
+        soma[i] = soma[i] + 1
+        bool = 0
+      }
     }
+  }
 }
+
+i # conferir se chegou em 10000
+
+
