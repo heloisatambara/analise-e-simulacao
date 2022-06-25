96bdb3061c71f64d6dffc9caef899b6af3d776cb
Heloísa Tambara <81993336+heloisatambara@users.noreply.github.com>
2022-06-25 09:02:45 -0300

    Update lista7.R

--- lista7.R
+++ lista7.R
@@ -6,11 +6,13 @@
 #### Exercicio 1 ####
 
 # Formula
-1 - (q/p)
-
+P = c(0,0,0,0)
+for (i in 1:4) {
+  P[i] = (1 - (q/p)^X[i])/(1-(q/p)^50)
+}
+plot(c(1,2,3,4),P )
 
 # a.
-
 ## Parametros iniciais
 X = c(10, 20, 30, 40)
 p = 18/38
@@ -26,12 +28,12 @@ for (i in 1:4){
     x = X[i]
     bool = 1
     while (bool) {
-      result = sample(c(-1,1), 1, replace = TRUE, prob = c(20/38, 18/38))
+      result = sample(c(-1,1), 1, replace = TRUE, prob = c(p, q))
       x = x + result
       if (x==0) {
         soma2 = soma2 + 1 # para conferir
         bool = 0
-      }
+      } 
       if (x==50) {
         soma[i] = soma[i] + 1
         bool = 0
@@ -39,7 +41,37 @@ for (i in 1:4){
     }
   }
 }
+soma = soma/1000
+i # conferir se chegou em 1000
+plot(c(1,2,3,4), soma)
+# b
+# rodar tudo de novo com
+p=1/2
 
-i # conferir se chegou em 10000
+#### Exercicio 2 ####
+# a
+N = c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+eventos = 0
+tempo = 0
+for (n in N) {
+  x = dexp(1:n,1)
+  a = runif(1000,x)
+  tempo = tempo + T
+  eventos = eventos + 1
+} 
+dpois(x, 1)
+
+plot(c(1:10),x)
 
 
+# b
+tempo = 0 
+eventos = 0
+for (i in 1:5) {
+  while (tempo < 100) {
+    T = rexp(1,1)
+    tempo = tempo + T
+    eventos = eventos + 1
+  }
+}
+?runif
